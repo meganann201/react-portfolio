7ea4412c05414ff0bfc188015a22cdbc1866aa10
Megan Campbell <megancampbell201@gmail.com>
2022-05-15 09:09:10 -0400

    link resume to google drive resume file

---
+++ src/components/Resume/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-md-8'>
+      <iframe src="https://drive.google.com/file/d/1Zuvx3VgF8rTZdoYoh6Xuptk0UrFClOFX/preview" width="640" height="640" allow="autoplay"></iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Resume;
