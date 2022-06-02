18c09c917f5fcafb5f7db41d89f73849a25097ed
Megan Campbell <megancampbell201@gmail.com>
2022-06-02 19:52:34 -0400

    added latest project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,11 +18,11 @@ function Portfolio() {
       repo: 'https://github.com/meganann201/imbd'
     },
     {
-      name: "Weather Dashboard",
-      description: "This app was created using the OpenWeather API. This app allows users to search names of cities for current weather data and a detailed 5 day forecast. Upon searching the name of the city it will be saved under the search input along with any other cities searched.",
-      image: 'https://i.imgur.com/s23SlRD.jpg',
-      link: 'https://meganann201.github.io/weather-dashboard/',
-      repo: 'https://github.com/meganann201/weather-dashboard'
+      name: "Eagle Fitness",
+      description: "A full Stack APP to track gym/trainer clients. This is a full stack application, hosted on heroku, that utilizes Node.js, Express.js, MongoDB, various NPM packages and various full stack web development concepts to create a fitness trakcer style website.",
+      image: 'https://raw.githubusercontent.com/meganann201/eagle-fitness/main/client/public/images/screenshot_1.png',
+      link: 'https://eagle-fitness.herokuapp.com/',
+      repo: 'https://github.com/meganann201/eagle-fitness'
     },
   ];
   const [projects, setProjects] = useState(initialProjects);
